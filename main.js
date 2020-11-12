5a7461a187d8f82dc9119dc047b3afcb23ccc0cf
Ravie Kattaura <ravie@keyuris.com>
2020-11-12 13:59:25 -0800

    updated check button

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
             '<li>' + '<span class="shopping-item">' + $("#shopping-list-entry").val() 
             + '</span>'  
             + '<button class="shopping-item-toggle">' + '<span class="button-label">' 
-            + 'check' + '</spam></button>' 
+            + 'check' + '</span></button>' 
             + '<button class="shopping-item-delete">' + '<span class="button-label">'
-            + 'delete' + '</spam></button></li>'
+            + 'delete' + '</span></button></li>'
 
         );
 
@@ -23,8 +23,42 @@ $(document).ready(function() {
     });
 
     // check items in list
-    $('.shopping-list').on('click', '.shopping-items-toggle', function(){
-        console.log('hello toggle');
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(){
         $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
 });
+
+/*
+
+Better approach to appending HTML
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
+
+<!-- In HTML -->
+<template id="new-shopping-item">
+    <li>
+        <span class="shopping-item">
+            <!-- {NEW_SHOPPING_ITEM} -->
+        </span>  
+        <button class="shopping-item-toggle">
+            <span class="button-label"> 
+                check
+            </span>
+        </button> 
+        <button class="shopping-item-delete">
+            <span class="button-label">
+                delete
+            </span>
+        </button>
+    </li>
+</template>
+
+<!-- In JS -->
+var template = document.querySelector('#new-shopping-item');
+
+// Clone the new row and insert it into the table
+var clone = template.content.cloneNode(true);
+var shopping_item = clone.querySelector(".shopping-item");
+
+shopping_item.textContent = $("#shopping-list-entry").val();
+$('.shopping-list').append(clone);
+*/
